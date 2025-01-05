a598e1a71b403f1b09373810119abd79bb5fc42b
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2025-01-05 09:20:16 -0300

    Classe ConversorMoedas refatorada

--- CurrencyConverter/src/main/java/com/conversormoedas/ConversorMoedas.java
+++ CurrencyConverter/src/main/java/com/conversormoedas/ConversorMoedas.java
@@ -15,7 +15,7 @@ public class ConversorMoedas {
     private static final String API_KEY = "e4a37c63c0e42d3c16bffb20";
     private static final String API_URL = "http://v6.exchangerate-api.com/v6/";
 
-    public void converte(String deMoeda, String paraMoeda) {
+    public void converte(String deMoeda, String paraMoeda, double quantia) {
         String urlString = API_URL + API_KEY + "/pair/" + deMoeda + "/" + paraMoeda;
 
         HttpClient client = HttpClient.newHttpClient();
@@ -32,7 +32,7 @@ public class ConversorMoedas {
                 double taxaConversao = json.get("conversion_rate").getAsDouble();
                 double quantiaConvertida = quantia * taxaConversao;
 
-                DecimalFormat df = DecimalFormat("#.##");
+                DecimalFormat df = new DecimalFormat("#.##");
                 System.out.println("Valor convertido de " + quantia + " " + deMoeda + " para " + paraMoeda + ": " + df.format(quantiaConvertida));
             } else {
                 System.out.println("Erro na conexao com a API. Codigo de sttatus: " + response.statusCode());
